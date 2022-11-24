8b39264c677fcaacb9e7f44f655a092ce77ddaed
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2022-11-24 17:35:55 +0100

    seaborn plot

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import plotly.graph_objects as go
 import pandas as pn
 import numpy as np
+import kaleido
+import seaborn as sns
+import matplotlib.pyplot as plt
 from plotly.graph_objs.histogram import XBins
 
 
@@ -8,6 +11,16 @@ from plotly.graph_objs.histogram import XBins
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def plotSnsHist(columnName, dfs: list, numBins=None):
+    numBins = 'auto' if numBins is None else numBins
+    for df in dfs:
+        sns.histplot(data=df, x=columnName, bins=numBins, element="step", fill=False,
+                     label=df.name + ' (' + str(len(df)) + ')')
+    plt.legend()
+    plt.savefig(columnName + '.png')
+    # plt.show()
+    plt.close()
 
 
 def plot(columnName, dfs: list, numbins=None):
@@ -24,8 +37,9 @@ def plot(columnName, dfs: list, numbins=None):
                       barmode='overlay'
                       )
     fig.show()
-    # fig.write_image(file=columnName + '.pdf', format='pdf')
-    # fig.write_image(file=columnName + '.png', format='png')
+    fig.write_image(file=columnName + '.pdf', format='pdf')
+    fig.write_image(file=columnName + '.png', format='png')
+
 
 def print_hi():
     min2 = pn.read_csv('D:/sciebo/Promotion/Auswertungen/20221027 - Mastermodul/20221014 - Pigmente/positiv/min2.csv',
@@ -45,13 +59,14 @@ def print_hi():
 
     dfs = [standard, min2, min3, min4]
 
-    plot('explained_intensity', dfs, 20)
-    plot('explained_peaks', dfs, 20)
+    # plot('explained_intensity', dfs, 20)
+    # plot('explained_peaks', dfs, 20)
+    # plot('num_peaks', dfs)
 
-    min2 = min2[min2['num_peaks'] >= 3]
-    min3 = min3[min3['num_peaks'] >= 3]
-    min4 = min4[min4['num_peaks'] >= 3]
-    standard = standard[standard['num_peaks'] >= 3]
+    min2 = min2[min2['num_peaks'] >= 2]
+    min3 = min3[min3['num_peaks'] >= 2]
+    min4 = min4[min4['num_peaks'] >= 2]
+    standard = standard[standard['num_peaks'] >= 2]
 
     min2.name = "min2"
     min3.name = "min3"
@@ -60,11 +75,31 @@ def print_hi():
 
     dfs = [standard, min2, min3, min4]
 
-    plot('explained_intensity', dfs, 20)
-    plot('explained_peaks', dfs, 20)
+    # plot('explained_intensity', dfs, 20)
+    # plot('explained_peaks', dfs, 20)
     # plot('num_peaks', dfs)
     # plot('spectral_entropy', dfs)
     # plot('normalized_entropy', dfs)
+    polarity = "Positive"
+    plt.title(polarity)
+    plotSnsHist('explained_intensity', dfs, [0.0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.0])
+    plt.title(polarity)
+    plotSnsHist('explained_peaks', dfs, [0.0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.0])
+    plt.title(polarity)
+    plotSnsHist('num_peaks', dfs)
+    plt.title(polarity)
+    plotSnsHist('spectral_entropy', dfs, 10)
+    plt.title(polarity)
+    plotSnsHist('normalized_entropy', dfs, [0.0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.0])
+
+    plt.title(polarity)
+    sns.scatterplot(data=standard, x="num_peaks", y="explained_peaks",
+                    label=standard.name + "(" + str(len(standard)) + ")")
+    sns.scatterplot(data=min2, x="num_peaks", y="explained_peaks", label=min2.name + "(" + str(len(min2)) + ")")
+    sns.scatterplot(data=min3, x="num_peaks", y="explained_peaks", label=min3.name + "(" + str(len(min3)) + ")")
+    sns.scatterplot(data=min4, x="num_peaks", y="explained_peaks", label=min4.name + "(" + str(len(min4)) + ")")
+    plt.legend()
+    plt.savefig("scatter_peaks_num_vs_explained.png")
 
 
 # Press the green button in the gutter to run the script.
